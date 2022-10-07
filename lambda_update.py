3e57c641ee8fd6a4d251e60091fa3626dd7398e3
Chxn-at-812627843742 <ojichinedu4@gmail.com>
2022-10-07 23:56:10 +0100

    added lambda function

---
+++ lambda_update.py
@@ -0,0 +1,42 @@
+import boto3
+import json
+
+def view_items():
+    client= boto3.client('dynamodb')
+    
+    response= client.get_item(
+       TableName= 'VisitorCounter',
+       Key= {
+           'Version': {
+               'N': '1'
+           }
+       },
+       ProjectionExpression= 'TotalCount'
+        )
+        
+    for Item in response:
+       return response['Item']['TotalCount']
+    
+
+
+def dynamo_action():
+    client = boto3.client('dynamodb')
+
+    client.update_item(
+        TableName='VisitorCounter',
+        Key= { 
+            'Version': { 
+                'N': '1'
+            }
+        },
+        UpdateExpression="SET TotalCount = TotalCount + :v",
+        ExpressionAttributeValues={
+            ':v': { 'N': '1'}
+        },
+        ReturnValues="UPDATED_NEW"
+    )
+    
+
+def update(event, context):
+    dynamo_action()
+    return view_items()
