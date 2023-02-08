bc8335f96c822ebd28a65353c80bcf78445f3263
Chxn-at-812627843742 <ojichinedu4@gmail.com>
2023-02-08 16:03:08 +0100

    fixed tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,6 @@
 describe('Counter API Testing', () => {
   it('fetches the current count - GET', () => {
-    cy.request('https://oresm0aeck.execute-api.us-east-1.amazonaws.com/update_count').as('countRequest');
+    cy.request('https://jl0b5q4gk9.execute-api.us-east-1.amazonaws.com').as('countRequest');
     cy.get('@countRequest').then( count => {
       expect(count.status).to.eq(200);
       expect(count.body.N).to.be.a('string')
